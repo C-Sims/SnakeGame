22711a7f91de5f0e31a363b5d27fa83c308e7322
C-Sims <christiansims00@gmail.com>
2025-01-20 12:10:53 +0100

    version 0.1

---
+++ src/snakeproject/SnakeProject.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package snakeproject;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author horabaixa
+ */
+public class SnakeProject extends Application {
+    
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = WIDTH;
+    private static final int ROWS = 20;
+    private static final int COLUMNS = ROWS;
+    private static final int SQUARE_SIZE = WIDTH / ROWS;
+    private static final String APPLE_IMAGE = "/img/apple.png";
+    
+    private static final int RIGHT = 0;
+    private static final int LEFT = 1;
+    private static final int UP = 2;
+    private static final int DOWN = 3;
+    
+    private GraphicsContext gc;
+    private List<Point> snakeBody = new ArrayList<>();
+    private Point snakeHead;
+    private Image foodImage;
+    private int foodX;
+    private int foodY;
+    private boolean gameOver;
+    private int currentDirection;
+   
+    
+    @Override
+    public void start(Stage primaryStage) {
+                  
+        primaryStage.setTitle("Snake");
+        
+        Group root = new Group();
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
